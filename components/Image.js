a375aaa9ba14bfb23c5fc43f62a72f705d980c6f
Logan <loganwbertrand@gmail.com>
2023-01-17 12:40:31 -0800

    image optimization

---
+++ components/Image.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Image from "next/image"
+
+export const ImageProfile = (props) => {
+	return (
+		<Image
+			src={"/images/new-profile.jpg"}
+			alt={props.alt}
+			width={props.width}
+			height={props.height}
+			style={{
+				borderRadius: "50%",
+			}}
+			objectFit="cover"
+		/>
+	)
+}
