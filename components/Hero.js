3fd87f4677846cd0fcf2a4b4da7f1cff46366663
Logan <loganwbertrand@gmail.com>
2024-02-20 20:22:37 -0800

    updated portfolio for bigger focus on product management

--- components/Hero.js
+++ components/Hero.js
@@ -88,7 +88,7 @@ const Hero = () => {
 
 					<Fade>
 						<About>
-							Hi! My name is Logan Bertrand, and I am a software
+							{/* Hi! My name is Logan Bertrand, and I am a software
 							developer with a passion for working with React and
 							React Native. I particularly enjoy front-end
 							development and creating intuitive and engaging user
@@ -101,19 +101,32 @@ const Hero = () => {
 							innovative solutions. If you're looking for a
 							developer who can bring both technical expertise and
 							a business-oriented mindset to your team, I would
-							love to chat!
+							love to chat! */}
+							Hey there! I'm Logan Bertrand, a seasoned
+							professional adept in project and product
+							management. My journey began in the vibrant world of
+							software development, where I found my niche in
+							crafting seamless user experiences using React and
+							React Native. With a keen focus on front-end
+							development, I thrive on building intuitive
+							interfaces that captivate users and drive
+							engagement. What sets me apart is my unique blend of
+							technical prowess and business acumen. Armed with a
+							Bachelor's in Technological Entrepreneurship and
+							Management, I bring a strategic perspective to my
+							development endeavors. I firmly believe that
+							technology should not only solve problems but also
+							enhance the user journey, and my background empowers
+							me to do just that. Driven by a passion for
+							innovation, I'm always on the lookout for fresh
+							challenges and collaborative opportunities. Whether
+							it's leading sprint planning sessions or conducting
+							customer interviews to glean insights, I'm committed
+							to delivering results that exceed expectations.
 						</About>
 						<br />
 						<About>
 							For a more in depth look of my background, visit my{" "}
-							<a
-								href="https://github.com/loganbertrand"
-								style={{ color: "black" }}
-								target="_blank"
-							>
-								GitHub
-							</a>{" "}
-							and{" "}
 							<a
 								href="https://www.linkedin.com/in/logan-bertrand-/"
 								style={{ color: "black" }}
